7ee667d3ea9efbdd6677a2caef32de64b2f8d0e8
tomsander1998 <tom.sander@polytechnique.edu>
2021-07-19 15:20:02 +0200

    erasing comments

--- src/RL/representation/default/defaultstaterepresentation.jl
+++ src/RL/representation/default/defaultstaterepresentation.jl
@@ -84,19 +84,3 @@ Returns the length of the feature vector, for the `DefaultFeaturization`.
 feature_length(::Type{<:FeaturizedStateRepresentation{DefaultFeaturization, TS}}) where TS = 3
 
 DefaultStateRepresentation(m::CPModel) = DefaultStateRepresentation{DefaultFeaturization, DefaultTrajectoryState}(m::CPModel)
-
-"""
-
-function print_tripartite(sr::DefaultStateRepresentation)
-    cpmodel = sr.cplayergraph
-    n = cpmodel.totalLength
-    nodefillc = []
-    for id in 1:n
-        v = cpmodel.idToNode[id]
-        if isa(v, VariableVertex) push!(nodefillc,"red")
-        elseif isa(v, ValueVertex) push!(nodefillc,"blue")
-        else  push!(nodefillc,"black") end
-    end
-    display(gplot(cpmodel;nodefillc=nodefillc))
-end
-"""
